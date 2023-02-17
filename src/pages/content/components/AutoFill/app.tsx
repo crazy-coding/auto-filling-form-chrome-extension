32072ce05f332a868583d8f3cf85c18f0afa2377
crazy-coding <ubisemin@gmail.com>
2023-02-17 16:01:55 -0500

    update ux

--- src/pages/content/components/AutoFill/app.tsx
+++ src/pages/content/components/AutoFill/app.tsx
@@ -1,12 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { eventOn } from ".";
 
 export default function App() {
   const [formData, setFormData] = useState([]);
+  const [profile, setProfile] = useState({});
+  const [focusedRef, setFocusedRef] = useState({});
+  const [open, setOpen] = useState(false);
 
   const autoFillingForm = () => {
     if (formData.length > 0) {
       const inputRefs = document.querySelectorAll('input, textarea');
-      console.log(inputRefs)
+
+      if (inputRefs.length < 1) {
+        setTimeout(autoFillingForm, 5000)
+      }
+
       inputRefs.forEach((inputRef) => {
         
         const refIds = inputRef.getAttributeNames().map((attrName) => inputRef.getAttribute(attrName));
@@ -34,10 +42,11 @@ export default function App() {
         // console.log("----------------------------------------", refIds)
         refIds.every((refId) => {
           formData.every((formD) => {
-            console.log("refId - checking", formD.ids, refId)
+            // console.log("refId - checking", formD.ids, refId)
             if (formD.ids.some((idKey) => refId.toLowerCase().indexOf(idKey.toLowerCase()) !== -1)) {
-              console.log("refId - found", formD.ids, refId)
-              inputRef.value = typeof formD.value !== 'string' ? formD.value[0]: formD.value;
+              // console.log("refId - found", formD.ids, refId)
+              (inputRef as HTMLInputElement).value = typeof formD.value !== 'string' ? formD.value[0]: formD.value;
+              inputRef.dispatchEvent(new Event("change"));
               return false;
             }
             return true;
@@ -48,10 +57,11 @@ export default function App() {
         // console.log("----------------------------------------", refLabels)
         refLabels.every((refLabel) => {
           formData.every((formD) => {
-            console.log("refLabel - checking", formD.labels, refLabel)
+            // console.log("refLabel - checking", formD.labels, refLabel)
             if (formD.labels.some((labelKey) => refLabel.toLowerCase().indexOf(labelKey.toLowerCase()) !== -1)) {
-              console.log("refLabel - found", formD.labels, refLabel)
-              inputRef.value = typeof formD.value !== 'string' ? formD.value[0]: formD.value;
+              // console.log("refLabel - found", formD.labels, refLabel)
+              (inputRef as HTMLInputElement).value = typeof formD.value !== 'string' ? formD.value[0]: formD.value;
+              inputRef.dispatchEvent(new Event("change"));
               return false;
             }
             return true;
@@ -64,9 +74,21 @@ export default function App() {
 
   useEffect(() => {
     chrome.storage.sync.get(['formData', 'profile']).then((result) => {
+      setProfile(result.profile);
       const filledFormData = result.formData.map((formD) => ({ ...formD, value: (result.profile[formD.id] || "") }))
       setFormData(filledFormData);
     });
+
+    eventOn('focusin', 'input, textarea', (e) => {
+      setFocusedRef(e);
+      setOpen(true);
+    })
+
+    eventOn('focusout', 'input, textarea', (e) => {
+      setTimeout(() => {
+        setOpen(false);
+      }, 200)
+    })
   }, []);
 
   useEffect(() => {
@@ -75,5 +97,25 @@ export default function App() {
     }
   }, [formData]);
 
-  return <div className="content-view">Sample</div>;
+  const onClick = (e) => {
+    (focusedRef as HTMLInputElement).value = e.target.textContent;
+    setFocusedRef(null);
+    setOpen(false);
+  }
+
+  return <div className="content-view">
+    {open && <div className="AutoFillModal">
+      <ul>
+        {Object.keys(profile).map((pro) => typeof profile[pro] !== "string" ? (
+          <>
+            {profile[pro].map((pr) => (
+              <li key={pr} onClick={onClick}>{typeof pr !== "string" ? JSON.stringify(pr) : pr}</li>
+            ))}
+          </>
+        ) : (
+          <li key={pro} onClick={onClick}>{typeof profile[pro] !== "string" ? JSON.stringify(profile[pro]) : profile[pro]}</li>
+        ))}
+      </ul>
+    </div>}
+  </div>;
 }
--- src/pages/content/components/AutoFill/index.tsx
+++ src/pages/content/components/AutoFill/index.tsx
@@ -8,4 +8,12 @@ const root = document.createElement("div");
 root.id = "chrome-extension-boilerplate-react-vite-content-view-root";
 document.body.append(root);
 
+export function eventOn(eventType, selector, callback) {
+  document.body.addEventListener(eventType, function (event) {
+    if (event.target.matches(selector)) {
+      callback(event.target);
+    }
+  });
+}
+
 createRoot(root).render(<App />);
